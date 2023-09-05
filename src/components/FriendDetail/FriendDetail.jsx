2a39998ce94141c45ce5397d80e7e158f58c8162
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-06 00:18:34 +0600

    friend go back btn added

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import './FriendDetail.css'
 
 const FriendDetail = () => {
     const friend = useLoaderData();
-
+const navigate = useNavigate();
+const goback=()=>{
+    navigate(-1)
+}
     return (
         <div className='frienddetail'>
             <h2>Everything about this person ID: {friend.id}</h2>
             <h4>Name: {friend.name}</h4>
             <h5>User Name: {friend.username}</h5>
             <h5>Website: {friend.website}</h5>
+            <button onClick={goback}>Go Back</button>
         </div>
     );
 };
