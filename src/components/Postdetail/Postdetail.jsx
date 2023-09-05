145b980669e375c1a954bc0b6a0cd8d0494212cc
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-05 12:53:58 +0600

    go back button added

--- src/components/Postdetail/Postdetail.jsx
+++ src/components/Postdetail/Postdetail.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import './Postdetail.css';
 
 const Postdetail = () => {
   const post = useLoaderData();
+  const navigate = useNavigate();
+  const goback =()=>{
+    navigate(-1)
+  }
   const { title, body, id } = post;
   return (
     <div className="postdetail">
       <h3>Post Details for: {id}</h3>
       <h3>{title}</h3>
       <p>{body}</p>
+      <button className="goback" onClick={goback}>Go Back</button>
     </div>
   );
 };
--- src/components/Singlepost/Singlepost.jsx
+++ src/components/Singlepost/Singlepost.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Singlepost.css'
 
 
 const Singlepost = ({post}) => {
-    const {id, title, body}=post
+    const {id, title}=post;
+    const navigate =useNavigate()
+    const handelnavigate=()=>{
+        navigate(`/post/${id}`);
+    }
     return (
         <div className='singlepost'>
             <h4>Id: {id}</h4>
             <h3>{title}</h3>
             <Link to={`/post/${id}`}>Show Details</Link>
-            <button className='showpostdetailbtn'>Show Post Details</button>
+            <button className='showpostdetailbtn' onClick={handelnavigate}>Show Post Details</button>
         </div>
     );
 };
