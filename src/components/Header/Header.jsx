cbbd7a5ee80ca9f986463154f027375257f5085a
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-05 13:13:04 +0600

    active link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/post'>Post</Link>
-            <Link to='/friends'>Friends</Link>
+            <ActiveLink to='/'>Home</ActiveLink>
+            <ActiveLink to='/about'>About</ActiveLink>
+            <ActiveLink to='/contact'>Contact</ActiveLink>
+            <ActiveLink to='/post'>Post</ActiveLink>
+            <ActiveLink to='/friends'>Friends</ActiveLink>
         </nav>
     );
 };
