1b98e4c35142c8cd8f7824cf72f09c438c5b2671
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-05 13:30:21 +0600

    loading spinner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
+  const navigation =useNavigation()
   return (
     <div>
       <Header></Header>
+      {
+        navigation.state === "loading" ? <FontAwesomeIcon height='80px' width='80px' icon={faSpinner} /> : ""
+      }
       <Outlet></Outlet>
     </div>
   );
